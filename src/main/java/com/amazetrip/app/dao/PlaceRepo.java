231592258a11ad9b9b3d2af812dc4907e41991e2
Octodex <cyril.brisson@etu.univ-amu.fr>
2021-10-18 15:26:01 +0200

    Add CRUD Place Repo

--- src/main/java/com/amazetrip/app/dao/PlaceRepo.java
+++ src/main/java/com/amazetrip/app/dao/PlaceRepo.java
@@ -1,2 +1,8 @@
-package com.amazetrip.app.dao;public class PlaceRepo {
+package com.amazetrip.app.dao;
+
+import com.amazetrip.app.model.Place;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PlaceRepo extends CrudRepository<Place, Integer> {
+
 }
