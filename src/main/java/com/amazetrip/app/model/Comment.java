c1d5c1befeaf0fbb88467844094c916a5e63080e
César <cesar.cardinale@etu.univ-amu.fr>
2021-10-18 14:44:54 +0200

    All Model

---
+++ src/main/java/com/amazetrip/app/model/Comment.java
@@ -0,0 +1,62 @@
+package com.amazetrip.app.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="comment_id")
+    private int id = 0;
+
+    @ManyToOne()
+    @JoinColumn(name = "trip_id")
+    private Trip trip;
+
+    @ManyToOne()
+    @JoinColumn(name = "place_id")
+    private Place place;
+
+    @ManyToOne()
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private Date creationDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public Trip getTrip() {
+        return trip;
+    }
+
+    public void setTrip(Trip trip) {
+        this.trip = trip;
+    }
+
+    public Place getPlace() {
+        return place;
+    }
+
+    public void setPlace(Place place) {
+        this.place = place;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/trips.jsp
@@ -0,0 +1,15 @@
+<%@ include file="./header.jsp"%>
+
+<div class="container">
+	<h1>Liste des films </h1>
+
+	<table class="table">
+		<tr>
+			<th>Nom</th>
+			<th>Année</th>
+			<th>Actions</th>
+		</tr>
+	</table>
+</div>
+
+<%@ include file="./footer.jsp"%>
