b67741854287364f6d57ae9aec6b30ac20e59e47
tuyendang3006 <45105732+jesminedang@users.noreply.github.com>
2021-10-23 19:46:30 +0200

    Add files via upload
    Add controller of user

---
+++ src/main/java/com/amazetrip/app/controller/UserController.java
@@ -0,0 +1,61 @@
+package com.amazetrip.app.controller;
+
+import com.amazetrip.api.dao.UserRepo;
+import com.amazetrip.api.model.User;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.servlet.ServletContext;
+
+@Controller()
+@RequestMapping("/app")
+public class UserController {
+    protected final Log logger = LogFactory.getLog(getClass());
+
+    @Autowired
+    ServletContext context;
+
+    @Autowired
+    UserRepo userRepo;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Start " + this);
+        if (userRepo.count() == 0) {
+            userRepo.save(new User("Durand", "Nicolas", "nicolas@gmail.com", "nicolas123"));
+            userRepo.save(new User("Roman", "Benoit", "benoit@yahoo.com", "benoit123"));
+            userRepo.save(new User("Hexa", "Alex", "alex@hotmail.fr", "alex123"));
+        }
+    }
+
+    @PreDestroy
+    public void destroy() {
+    }
+
+    /**
+     *  Afficher la liste d'utilisateurs
+     */
+    @RequestMapping("/users")
+    private ModelAndView getUsers(){
+        var res = new ModelAndView("users");
+        res.addObject("users", null);
+        return res;
+    }
+
+    /**
+     *  Afficher le profil
+     */
+    @RequestMapping("/profil/{id}")
+    private ModelAndView getProfil(@ModelAttribute User user){
+        var res = new ModelAndView("profil");
+        res.addObject("profil", user);
+        return res;
+    }
+}
